97e5fcfab319f4a09a21a0706f1d9af91594233c
Sergio M <sergio@example.com>
2023-09-27 15:27:14 -0400

    initial commit

---
+++ day04/day04.py
@@ -0,0 +1,36 @@
+
+
+with open('input.txt') as f:
+    lines = f.read().split('\n')
+
+
+def elf_sections(entry):
+    first_section, last_section = entry.split('-')
+    sections = list(range(int(first_section), int(last_section) + 1))
+    return sections
+
+
+def part1():
+    fully_contained_count = 0
+
+    for line in lines:
+        if not line:
+            continue
+
+        elf1, elf2 = line.split(',')
+        elf1_sections = elf_sections(elf1)
+        elf2_sections = elf_sections(elf2)
+
+        if (
+            all(section in elf2_sections for section in elf1_sections)
+            or all(section in elf1_sections for section in elf2_sections)
+        ):
+            fully_contained_count += 1
+
+    return fully_contained_count
+
+
+print(part1()) # first try woop woop!
+
+
+print(part2())
