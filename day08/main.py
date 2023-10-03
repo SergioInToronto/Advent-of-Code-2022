ec546236e7f3e1fd17a5b05f3e008b946b5280e0
Sergio M <sergio@example.com>
2023-10-03 14:26:41 -0400

    WIP day8

---
+++ day08/main.py
@@ -0,0 +1,13 @@
+
+
+lines = open('input.txt').read().strip().split('\n')
+
+
+# part1: how many trees are visible from the outside?
+
+def trees_above_and_below(x, y):
+    pass
+
+
+def trees_left_and_right(x, y):
+    pass
