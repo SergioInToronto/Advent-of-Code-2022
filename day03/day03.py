8865192f38d4a874cdff8f715ecfe10e3424d478
Sergio M <sergio@example.com>
2023-10-09 17:20:37 -0400

    day3 part2 in Rust woohoo!

--- day03/day03.py
+++ day03/day03.py
@@ -5,14 +5,11 @@ ITEM_PRIORITIES = dict(zip(string.ascii_letters, range(1, 53)))
 
 
 with open('input.txt') as f:
-    lines = f.read().split('\n')
+    lines = f.read().strip().split('\n')
 
 total_priorities = 0
 
 for rucksack in lines:
-    if not rucksack:
-        continue
-
     middle_point = int(len(rucksack) / 2)
     items1, items2 = rucksack[:middle_point], rucksack[middle_point:]
     misplaced_item = next(x for x in items1 if x in items2)
--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,6 +1,9 @@
 use std::fs;
 
 
+const LETTERS: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+
 fn find_common_item(items1: &str, items2: &str) -> char {
     for x in items1.chars() {
         if items2.contains(x) {
@@ -11,12 +14,18 @@ fn find_common_item(items1: &str, items2: &str) -> char {
     return '_';
 }
 
+fn find_common_item_of_3(items1: &str, items2: &str, items3: &str) -> char {
+    for x in items1.chars() {
+        if items2.contains(x) && items3.contains(x) {
+            // println!("Common item: {}", &x);
+            return x.clone();
+        }
+    }
+    return '_';
+}
 
-fn main() {
-    const LETTERS: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    let contents = fs::read_to_string("input.txt").expect("Failed to read file");
-
+fn part1(contents: &String) {
     let mut total_priorities = 0;
     for row in contents.trim_end().split("\n") {
         let midpoint: usize = &row.len() / 2;
@@ -32,5 +41,36 @@ fn main() {
         // println!("Items1: {}, items2: {}", &items1, &items2);
     }
 
-    println!("Total priorities: {}", total_priorities);
+    println!("Part1 - Total priorities: {}", total_priorities);
+}
+
+
+fn part2(contents: &String) {
+    // let line_count = contents.trim_end().split("\n").collect().len();
+    // dbg!(line_count);
+    let mut total_priorities = 0;
+    let mut bags_iter = contents.trim_end().split_whitespace();
+    loop { // TODO: use .iter().step_by(3) instead ??
+        let bag1 = bags_iter.next();
+        let bag2 = bags_iter.next();
+        let bag3 = bags_iter.next();
+        if bag1 == None || bag2 == None || bag3 == None {
+            break
+        }
+        let badge = find_common_item_of_3(&bag1.unwrap(), &bag2.unwrap(), &bag3.unwrap());
+        let priority = LETTERS.find(badge).unwrap() + 1;
+        total_priorities = total_priorities + priority;
+        // println!("Group Badge: {}", &badge);
+    }
+
+    println!("Part2 - Total badge priorities: {}", total_priorities);
+}
+
+
+fn main() {
+    let contents = fs::read_to_string("input.txt").expect("Failed to read file");
+
+    part1(&contents);
+
+    part2(&contents);
 }
