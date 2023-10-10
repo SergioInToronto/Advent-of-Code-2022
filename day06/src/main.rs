23a8408edd7a1b027a2c90712b04773c1332cf30
Sergio M <sergio@example.com>
2023-10-10 15:02:09 -0400

    day6 part2

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -5,25 +5,24 @@ use std::collections::HashSet;
 fn main() {
     let contents = fs::read_to_string("input.txt").expect("Failed to read file");
 
-    part1(&contents);
+    println!("Part1 - Start-of-packet marker found after character {}", part1(&contents, 4));
+    println!("Part2 - Start-of-message marker found after character {}", part1(&contents, 14));
 }
 
 
-fn part1(contents: &String) {
+fn part1(contents: &String, char_count: usize) -> usize{
     // let mut iter = contents.iter().peekable()
     // for char in iter {
     // }
 
     let mut slice: &str;
-    let end_idx = contents.len() - 4;
+    let end_idx = contents.len() - char_count;
     for start_idx in 0..end_idx {
-        slice = &contents[start_idx..(start_idx+4)];
+        slice = &contents[start_idx..(start_idx+char_count)];
         let set = slice.chars().collect::<HashSet<char>>();
-        if set.len() == 4 {
-            println!("Part1 - Start-of-packet marker found at offset {}", start_idx);
-            return;
+        if set.len() == char_count {
+            return start_idx + char_count;
         }
     }
+    return 0; // TODO: learn to throw exceptions
 }
-
-
